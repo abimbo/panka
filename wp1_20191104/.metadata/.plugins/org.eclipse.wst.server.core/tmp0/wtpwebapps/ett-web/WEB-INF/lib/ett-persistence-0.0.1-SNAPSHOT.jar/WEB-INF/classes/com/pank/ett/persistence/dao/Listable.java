35721a0ac877532fc2dd8b5cb7e268aadb2da114
abimbo <bimbo.andras@gmail.com>
2019-11-05 00:02:44 +0100

    20191105

--- wp1_20191104/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ett-web/WEB-INF/lib/ett-persistence-0.0.1-SNAPSHOT.jar/WEB-INF/classes/com/pank/ett/persistence/dao/Listable.java
+++
@@ -1,7 +0,0 @@
-package com.pank.ett.persistence.dao;
-
-import java.util.List;
-
-public interface Listable<BASETYPE> {
-	public List<BASETYPE> getList();
-}
--- wp1_20191104/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ett-web/WEB-INF/lib/ett-persistence-0.0.1-SNAPSHOT.jar/WEB-INF/classes/com/pank/ett/persistence/dao/PrDAO.java
+++
@@ -1,7 +0,0 @@
-package com.pank.ett.persistence.dao;
-
-import com.pank.ett.persistence.model.Pr;
-
-public interface PrDAO extends CRUD<Pr, Integer>, Listable<Pr>{
-	
-}
